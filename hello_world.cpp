3315a94600a4905fb8684e819139a51f682ff296
a0730c <a0730c@gmail.com>
2023-03-08 23:47:03 +0300

    Gentle named hello world

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   string name;
-  cout << "Enter your name"; 
+  cout << "Enter your name, please"; 
   cin << name;
   cout << "Hello, World from " << name << std::endl;
   return 0;
